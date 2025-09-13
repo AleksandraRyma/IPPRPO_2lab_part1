5eaf9b84b64c88413f5b6d624dedd3a9fc7b30d8
Aleksandra <aleksandra.ryma23@gmail.com>
2025-09-13 22:36:37 +0300

    Добавлено сообщение в ветке feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,8 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;public class HelloWorld {
     public static void main(String[] args) {
-        String text = "Привет, мир!, Hello Vitaly!";
-        try (OutputStreamWriter writer = new OutputStreamWriter(System.out, StandardCharsets.UTF_8)) {
-            writer.write(text);
-            writer.flush(); // Убедимся, что всё записано
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Hello, World!");
+        System.out.println("Some temporary changes!");
+
     }
 }
