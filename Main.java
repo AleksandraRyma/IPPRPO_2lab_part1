f9e7f8ab38b49ac91034810401a9e7764263e5b3
Aleksandra <aleksandra.ryma23@gmail.com>
2025-09-14 12:41:50 +0300

    Добавлен калькулятор в ветку master

--- Main.java
+++ Main.java
@@ -15,6 +15,38 @@ public class Main {
         choice = sc.nextInt();
         switch (choice) {
             case 1: System.out.println("1 - калькулятор");
+            System.out.print("Введите первое число num1 = ");
+            double num1 = sc.nextDouble();
+            System.out.println();
+            System.out.print("Введите второе число num2 = ");
+            double num2 = sc.nextDouble();
+            System.out.println();
+                System.out.println("Меню");
+                System.out.println("1 - сложение");
+                System.out.println("2 - вычитание");
+                System.out.println("3 - умножение");
+                System.out.println("4 - деление");
+                System.out.print("Ваш выбор: ");
+                int choice2 = sc.nextInt();
+                System.out.println();
+                double result = 0;
+                Calculator calc = new Calculator();
+                switch (choice2) {
+                case 1:
+                    result = calc.sum(num1, num2);
+                break;
+                case 2:
+                    result = calc.delete(num1, num2);
+                break;
+                case 3:
+                    result = calc.multiplication(num1, num2);
+                break;
+                case 4:
+                    result = calc.division(num1, num2);
+                break;
+                }
+                System.out.println("Ответ: " + result);
+
             break;
             case 2: System.out.println("2");
             break;
