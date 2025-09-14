d1569a26973bd91efe303f97cbeb0af8bbc6126d
Aleksandra <aleksandra.ryma23@gmail.com>
2025-09-14 13:39:06 +0300

    Проверка на одинаковые числа

--- MaxOfThree.java
+++ MaxOfThree.java
@@ -10,8 +10,13 @@ public class MaxOfThree {
 
         int max = a;
         if (b > max) max = b;
-        if (c > max) max = c;
+        if (c > max) max = b;
 
         System.out.println("Максимальное число: " + max);
+        if ((a == b && b == c)) {
+            System.out.println("Все три числа равны.");
+        } else if ((a == b && a == max) || (b == c && b == max) || (a == c && a == max)) {
+            System.out.println("Есть равные максимальные числа.");
+        }
     }
 }
