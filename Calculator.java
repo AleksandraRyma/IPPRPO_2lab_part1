59184b115ce6242ef153505394c025b2ac3ebccf
Aleksandra <aleksandra.ryma23@gmail.com>
2025-09-14 12:17:22 +0300

    Добавлена функ. умножение в ветку multiplication

--- Calculator.java
+++ Calculator.java
@@ -5,4 +5,7 @@ public class Calculator {
     public double delete(double num1, double num2) {
         return num1 - num2;
     }
+    public double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
 }
